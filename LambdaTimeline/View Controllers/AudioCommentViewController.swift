a09491f379fe098b6f902263bc63941391f99f68
fritzgt <tuanis@live.com>
2020-04-23 12:17:57 +0300

    completed audio recording VC

--- LambdaTimeline/View Controllers/AudioCommentViewController.swift
+++ LambdaTimeline/View Controllers/AudioCommentViewController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class AudioCommentViewController: UIViewController {
+    
+    //MARK: - Properties
+    private var recordingURL: URL?
+    private var audioRecorder: AVAudioRecorder?
+    private var isRecording: Bool {
+        //Check if is currently recoding and give a default value of false
+        return audioRecorder?.isRecording ?? false
+    }
 
     //MARK: - Outlets
     @IBOutlet weak var recordButton: UIButton!
@@ -18,16 +28,71 @@ class AudioCommentViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateViews()
         // Do any additional setup after loading the view.
     }
     
     //MARK: - Actions
     @IBAction func doneButtonPressed(_ sender: UIButton) {
+        audioRecorder?.stop()
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func toggleRecord(_ sender: UIButton) {
+        
+        
+        
+        guard !isRecording else {
+            //already recording then toggle to stop recording and return to block continuing
+            audioRecorder?.stop()
+            updateViews()
+            return
+        }
+        
+        
+        do{
+            let format = AVAudioFormat(standardFormatWithSampleRate: 44100.0, channels: 2)!
+            audioRecorder = try AVAudioRecorder(url: newRecordingURL(), format: format)
+            audioRecorder?.delegate = self
+            audioRecorder?.record()
+            updateViews()
+        }catch{
+            NSLog("Unable to start recording: \(error)")
+        }
     }
     
+    //MARK: - Custom Methods
+    
+    private func newRecordingURL()-> URL {
+        let fm = FileManager.default
+        let documentsDir = try! fm.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        
+        //MAKE A Unique path using UUID, The “caf” extension stands for Core Audio Format.
+        return documentsDir.appendingPathComponent(UUID().uuidString).appendingPathExtension("caf")
+    }
+    
+    func updateViews() {
+          recordButton.isSelected = isRecording
+     }
+}
 
+ 
+
+//MARK: - AVAudioRecorderDelegate
+
+extension AudioCommentViewController:AVAudioRecorderDelegate{
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        updateViews()
+        //get the url and save it to the var recordingURL
+        recordingURL = recorder.url
+        self.audioRecorder = nil
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(_ recorder: AVAudioRecorder, error: Error?) {
+        updateViews()
+        if let error = error{
+             NSLog("Error ecoder error: \(error)")
+        }
+    }
 }
