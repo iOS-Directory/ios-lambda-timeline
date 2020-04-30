181b102a052081b5e38a3f314b45db8299c71fa0
fritzgt <tuanis@live.com>
2020-04-30 16:42:31 +0300

    setup preview layer

--- LambdaTimeline/View Controllers/CameraPreviewView.swift
+++ LambdaTimeline/View Controllers/CameraPreviewView.swift
@@ -7,15 +7,21 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraPreviewView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
     }
-    */
+    
+    var videoPlayerView: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    var session: AVCaptureSession? {
+        get { return videoPlayerView.session }
+        set { videoPlayerView.session = newValue }
+    }
 
 }
