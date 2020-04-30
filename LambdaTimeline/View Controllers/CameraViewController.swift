7c439b2f8292ec5e259773903e7458dbbfc07a23
fritzgt <tuanis@live.com>
2020-04-30 16:49:10 +0300

    added bestCamera to choose camera

--- LambdaTimeline/View Controllers/CameraViewController.swift
+++ LambdaTimeline/View Controllers/CameraViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
 
     
     //MARK: - Properties
-    
+    lazy private var captureSession = AVCaptureSession()
     
     //MARK: - Oulets
     @IBOutlet weak var camaraView: CameraPreviewView!
@@ -25,6 +26,24 @@ class CameraViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    //MARK: - Custom Methods
+    
+    func setupCaptureSession() {
+        
+    }
+    
+    func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+                //If this camera is available then we return it
+                return device
+        }
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+                  //If this camera is available then we return it
+                  return device
+          }
+        fatalError("No cameras on the device (or you are running it on the iPhone simulator")
     }
     
 
