3d9a24d384522009b0701f34902150b96c361ba2
fritzgt <tuanis@live.com>
2020-04-30 14:47:34 +0300

    added permissions to plist and initial VC

--- LambdaTimeline/View Controllers/SignInViewController.swift
+++ LambdaTimeline/View Controllers/SignInViewController.swift
@@ -9,22 +9,78 @@
 import UIKit
 import Firebase
 import GoogleSignIn
+import AVFoundation
 
 class SignInViewController: UIViewController {
-
+    
+    //MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         GIDSignIn.sharedInstance()?.presentingViewController = self
         GIDSignIn.sharedInstance()?.delegate = self
-        
+        requestPermissionAndShowCamera()
         setUpSignInButton()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // TODO: get permission
+        
+        showCamera()
+        
+    }
+    
+    //MARK: - Actions
     @IBAction func googleSignIn(_ sender: Any) {
         GIDSignIn.sharedInstance()?.signIn()
     }
+    
+    //MARK: - Permission
+    private func requestPermissionAndShowCamera(){
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        switch status {
+            
+        case .notDetermined:
+            // First time user has seen the dialog, we don't have permission
+            
+            requestPermission()
+        case .restricted:
+            // parental controls
+            
+            fatalError("Video is disabled for parental controls")
+        case .denied:
+            // we asked for permission and they said no
+            
+            fatalError("Tell user they need to enable Privacy for Video/Camera/Microphone")
+        case .authorized:
+            // we asked for permission and they said yes
+            
+            showCamera()
+        default:
+            fatalError("A new status was added that we need to handle")
+        }
+    }
+    
+    private func requestPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (granted) in
+            guard granted else {
+                fatalError("Tell user they need to enable Privacy for Video/Camera/Microphone")
+            }
+            DispatchQueue.main.async { [weak self] in
+                self?.showCamera()
+            }
+        }
+    }
+    
+    //MARK: - Methods
+    private func showCamera() {
+        performSegue(withIdentifier: "ShowCamera", sender: self)
+    }
 }
 
+    //MARK: - GIDSignInDelegate
 extension SignInViewController: GIDSignInDelegate {
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         
@@ -74,3 +130,4 @@ extension SignInViewController: GIDSignInDelegate {
                              buttonWidthConstraint])
     }
 }
+
