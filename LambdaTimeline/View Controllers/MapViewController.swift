dd29c5a41abc1b7831a05e6e8b5a7c930e2e7a5d
fritzgt <tuanis@live.com>
2020-05-07 22:14:33 +0300

    Create mapview controller

---
+++ LambdaTimeline/View Controllers/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  LambdaTimeline
+//
+//  Created by FGT MAC on 5/7/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
